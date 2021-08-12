0680b4d24751e62b5a163690791e267d261cc4fc
Neonter <barkan.makar@gmail.com>
2021-08-12 15:58:27 +0300

    bug fix + removed build files

--- core/src/main/java/com/epam/Utils.java
+++ core/src/main/java/com/epam/Utils.java
@@ -5,7 +5,7 @@ import com.epam.util.StringUtils;
 public class Utils {
     public static boolean isAllPositiveNumbers(String... str) {
         for (String s : str) {
-            return StringUtils.isPositiveNumber(s);
+            if (!StringUtils.isPositiveNumber(s)) return false;
         }
         return true;
     }
